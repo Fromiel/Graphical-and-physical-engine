f4fd5ea303c261a88423cc5e221b2561cfe54604
JulesLinard <79532003+JulesLinard@users.noreply.github.com>
2022-09-07 21:34:17 +0200

    Début Vecteur3D

---
+++ Vecteur3D.cpp
@@ -0,0 +1,4 @@
+//
+// Created by jules on 07/09/2022.
+//
+
---
+++ Vecteur3D.h
@@ -0,0 +1,49 @@
+//
+// Created by jules on 07/09/2022.
+//
+
+#ifndef PROJET_MATHS_PHYSIQUES_JV_VECTEUR3D_H
+#define PROJET_MATHS_PHYSIQUES_JV_VECTEUR3D_H
+
+class Vecteur3D{
+
+    private :
+        double x;
+        double y;
+        double z;
+
+    public:
+
+        Vecteur3D(double x=0, double y=0, double z=0);
+
+        Vecteur3D(const Vecteur3D & vect);
+
+        double get_x() const {return x;}
+
+        double get_y() const {return y;}
+
+        double get_z() const {return z;}
+
+        double norm();
+
+        double norm_squared();
+
+        Vecteur3D normalized();
+
+        Vecteur3D scalar_multiplication(double scalar);
+
+};
+
+std::ostream & operator<<(std::ostream & out, const Vecteur3D & vect);
+
+Vecteur3D operator+(const Vecteur3D vect1, const Vecteur3D vect2);
+
+Vecteur3D operator-(const Vecteur3D vect1, const Vecteur3D vect2);
+
+Vecteur3D operator*(const Vecteur3D vect1, const Vecteur3D vect2);
+
+Vecteur3D scalar_product(const Vecteur3D vect1, const Vecteur3D vect2);
+
+Vecteur3D vectorial_product(const Vecteur3D vect1, const Vecteur3D vect2);
+
+#endif //PROJET_MATHS_PHYSIQUES_JV_VECTEUR3D_H
