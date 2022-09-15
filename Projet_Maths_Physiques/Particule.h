0d878938f345a5872466c22facd578e611106315
Mathxxl <mathieu.baesen@free.fr>
2022-09-15 15:34:15 -0400

    Update Particule.h

--- Projet_Maths_Physiques/Particule.h
+++ Projet_Maths_Physiques/Particule.h
@@ -6,18 +6,53 @@
 
 class Particule {
 public:
-	//Constructeurs
+	//Constructeurs//
+
 	Particule(Vecteur3D pos, Vecteur3D vel, Vecteur3D acc, float r, float m);
 	Particule(const Particule &p);
-	//Getters
+
+	//Getters//
+
+	/**
+	* Getter pour l'attribut InverseMasse de la classe
+	*/
 	float getInverseMasse() const { return InverseMasse; }
+
+	/**
+	* Getter pour l'attribut rayon de la classe
+	*/
 	float getRayon() const { return rayon; }
+
+	/**
+	* Getter pour l'attribut position de la classe
+	*/
 	Vecteur3D getPos() const { return position; }
+
+	/**
+	* Getter pour l'attribut velocity de la classe
+	*/
 	Vecteur3D getSpeed() const { return velocity; }
+
+	/**
+	* Getter pour l'attribut acceleration de la classe
+	*/
 	Vecteur3D getAcc() const { return acceleration; }
-	//Setters
+	
+	//Setters//
+	
+	/**
+	* Setter pour l'attribut position de la classe
+	*/
 	void setPos(const Vecteur3D);
+
+	/**
+	* Setter pour l'attribut velocity de la classe
+	*/
 	void setSpeed(const Vecteur3D);
+
+	/**
+	* Setter pour l'attribut acceleration de la classe
+	*/
 	void setAcceleration(const Vecteur3D);
 
 private:
