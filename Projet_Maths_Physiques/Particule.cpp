fec382a95b9124c9593e038c755faa099eefbb3b
Mathxxl <mathieu.baesen@free.fr>
2022-09-15 14:57:35 -0400

    Fine

--- Projet_Maths_Physiques/Particule.cpp
+++ Projet_Maths_Physiques/Particule.cpp
@@ -1,29 +1,31 @@
 #include "Particule.h"
 
+
 Particule::Particule(Vecteur3D pos, Vecteur3D s, Vecteur3D a, float r, float m) {
-	position = Vecteur3D(pos);
-	velocity = Vecteur3D(s);
-	acceleration = Vecteur3D(a);
+	position = pos;
+	velocity = s;
+	acceleration = a;
 	rayon = r;
 	InverseMasse = m;
 }
 
-Particule::Particule(const Particule& p) {
-	position = Vecteur3D(p.getPos());
-	velocity = Vecteur3D(p.getSpeed());
-	acceleration = Vecteur3D(p.getAcc());
+Particule::Particule(const Particule &p) {
+	position = p.getPos();
+	velocity = p.getSpeed();
+	acceleration = p.getAcc();
 	rayon = p.getRayon();
 	InverseMasse = p.getInverseMasse();
 }
 
 void Particule::setPos(const Vecteur3D pos) {
-	position = Vecteur3D(pos);
+	position = pos;
 }
 
 void Particule::setSpeed(const Vecteur3D s) {
-	velocity = Vecteur3D(s);
+	velocity = s;
 }
 
 void Particule::setAcceleration(const Vecteur3D a) {
-	acceleration = Vecteur3D(a);
+	acceleration = a;
 }
+
