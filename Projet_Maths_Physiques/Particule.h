597d3eca3934ca96de6582f44a304e0c56472061
Mathxxl <mathieu.baesen@free.fr>
2022-09-15 14:38:15 -0400

    Correction1 Particule

--- Projet_Maths_Physiques/Particule.h
+++ Projet_Maths_Physiques/Particule.h
@@ -10,11 +10,11 @@ public:
 	Particule(Vecteur3D pos, Vecteur3D vel, Vecteur3D acc, float r, float m);
 	Particule(const Particule &p);
 	//Getters
-	float getInverseMasse() const {return InverseMasse};
-	float getRayon() const {return rayon};
-	Vecteur3D getPos() const {return position};
-	Vecteur3D getSpeed() const {return velocity};
-	Vecteur3D getAcc() const {return acceleration};
+	float getInverseMasse() const { return InverseMasse; }
+	float getRayon() const { return rayon; }
+	Vecteur3D getPos() const { return position; }
+	Vecteur3D getSpeed() const { return velocity; }
+	Vecteur3D getAcc() const { return acceleration; }
 	//Setters
 	void setPos(const Vecteur3D);
 	void setSpeed(const Vecteur3D);
