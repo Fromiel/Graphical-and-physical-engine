200dfee5b60af5e033b70e0ac2287b15dff53e75
Mathxxl <mathieu.baesen@free.fr>
2022-09-15 14:32:25 -0400

    Classe Particule Générique

---
+++ Projet_Maths_Physiques/Particule.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef PARTICULE_H
+#define PARTICULE_H
+
+#include "Vecteur3D.h"
+
+class Particule {
+public:
+	//Constructeurs
+	Particule(Vecteur3D pos, Vecteur3D vel, Vecteur3D acc, float r, float m);
+	Particule(const Particule &p);
+	//Getters
+	float getInverseMasse() const {return InverseMasse};
+	float getRayon() const {return rayon};
+	Vecteur3D getPos() const {return position};
+	Vecteur3D getSpeed() const {return velocity};
+	Vecteur3D getAcc() const {return acceleration};
+	//Setters
+	void setPos(const Vecteur3D);
+	void setSpeed(const Vecteur3D);
+	void setAcceleration(const Vecteur3D);
+
+private:
+	Vecteur3D position;
+	Vecteur3D velocity;
+	Vecteur3D acceleration;
+	float rayon;
+	float InverseMasse;
+
+};
+
+#endif
