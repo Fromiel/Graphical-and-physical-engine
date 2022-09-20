22887d141798a075c0366328c2315d6235b2c828
Noam-NS <72453874+Noam-NS@users.noreply.github.com>
2022-09-19 21:36:01 -0400

    Ajout de la classe Matrix3D

---
+++ Projet_Maths_Physiques/includes/Matrix3D.h
@@ -0,0 +1,52 @@
+#ifndef MATRIX3D_H
+#define MATRIX3D_H
+
+#include <ostream>
+#include <vector>
+
+#include <algorithm>
+#include <functional>
+
+class Matrix3D {
+    private:
+        std::vector<double> content;
+
+    public:
+        static const int lineCount = 3;
+        static const int columnCount = 3;
+
+        Matrix3D();
+        Matrix3D(const std::vector<double> &content);
+        Matrix3D(const Matrix3D &matrix3D);
+        ~Matrix3D();
+
+        std::vector<double> getContentAsStdVector() const;
+        
+        double operator()(const int &line, const int &column) const;
+        void operator=(const Matrix3D &matrix3D);
+
+        Matrix3D operator+=(const Matrix3D &matrix3D);
+        Matrix3D operator-=(const Matrix3D &matrix3D);
+
+        double determinant() const;
+        Matrix3D normalize() const;
+        Matrix3D transpose() const;
+        Matrix3D adjugate() const;
+        Matrix3D invert() const;
+
+        static Matrix3D identity();
+};
+
+Matrix3D operator+(const Matrix3D &matrix3D_1, const Matrix3D &matrix3D_2);
+Matrix3D operator-(const Matrix3D &matrix3D_1, const Matrix3D &matrix3D_2);
+
+Matrix3D operator*(const Matrix3D &matrix3D, const double &multiplier);
+Matrix3D operator*(const double &multiplier, const Matrix3D &matrix3D);
+
+Matrix3D operator*(const Matrix3D &matrix3D_1, const Matrix3D &matrix3D_2);
+
+bool operator==(const Matrix3D &matrix3D_1, const Matrix3D &matrix3D_2);
+
+std::ostream &operator<<(std::ostream &out, const Matrix3D &matrix3D);
+
+#endif
