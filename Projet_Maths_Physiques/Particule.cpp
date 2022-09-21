d30673f9de952ee0c3a9edfe5ab7826c5af98c63
Mathxxl <mathieu.baesen@free.fr>
2022-09-21 14:25:22 -0400

    Physique de la particule

--- Projet_Maths_Physiques/Particule.cpp
+++ Projet_Maths_Physiques/Particule.cpp
@@ -1,17 +1,17 @@
 #include "Particule.h"
+#include <ctime>
 
-
-Particule::Particule(Vecteur3D pos, Vecteur3D s, Vecteur3D a, float r, float m) {
-	position = pos;
-	velocity = s;
-	acceleration = a;
+Particule::Particule(Vecteur3D pos_initiale, Vecteur3D v_initiale, double r, double m) {
+	position = pos_initiale;
+	velocity = v_initiale;
+	acceleration = 0;
 	rayon = r;
 	InverseMasse = m;
 }
 
 Particule::Particule(const Particule &p) {
 	position = p.getPos();
-	velocity = p.getSpeed();
+	velocity = p.getVelocity();
 	acceleration = p.getAcc();
 	rayon = p.getRayon();
 	InverseMasse = p.getInverseMasse();
@@ -21,11 +21,25 @@ void Particule::setPos(const Vecteur3D pos) {
 	position = pos;
 }
 
-void Particule::setSpeed(const Vecteur3D s) {
+void Particule::setVelocity(const Vecteur3D s) {
 	velocity = s;
 }
 
 void Particule::setAcceleration(const Vecteur3D a) {
 	acceleration = a;
+}
+
+void Particule::update() {
+	double h = time.h / ctime; 
+	updateVelocity(h);
+	updatePosition(h);
+}
+
+void Particule::updateVelocity(double frameRate) {
+	position = position + (frameRate * velocity);
+}
+
+void Particule::updatePosition(double frameRate) {
+	velocity = velocity + (frameRate * acceleration);
 }
 
---
+++ Projet_Maths_Physiques/includes/Particule.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef PARTICULE_H
+#define PARTICULE_H
+
+#include "Vecteur3D.h"
+
+class Particule {
+public:
+	//Constructeurs//
+
+	Particule(Vecteur3D pos_initiale, Vecteur3D vel_initiale, double r, double m);
+	Particule(const Particule &p);
+
+	//Getters//
+
+	double getInverseMasse() const { return InverseMasse; }
+	double getRayon() const { return rayon; }
+	Vecteur3D getPos() const { return position; }
+	Vecteur3D getVelocity() const { return velocity; }
+	Vecteur3D getAcc() const { return acceleration; }
+	
+	//Setters//
+	
+	void setPos(const Vecteur3D);
+	void setVelocity(const Vecteur3D);
+	void setAcceleration(const Vecteur3D);
+
+	/**
+	* Fonction qui mets à jour la position de la particule en fonction du temps
+	*/
+	void update();
+
+private:
+	Vecteur3D position;
+	Vecteur3D velocity;
+	Vecteur3D acceleration;
+	double rayon;
+	double InverseMasse;
+
+	static double g = 9.81;
+
+	//Fonctions d'update
+
+	/**
+	* Mets à jour la position de la particule en fonction du frame rate
+	*/
+	void updatePosition(double frameRate = 1/60);
+
+	/**
+	* Mets à jour la vélocité de la particule en fonction du frame rate
+	*/
+	void updateVelocity(double frameRate = 1/60);
+
+};
+
+#endif
