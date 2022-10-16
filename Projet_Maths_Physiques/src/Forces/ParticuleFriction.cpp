feaca1600b34101701376ad62168cee641cfbd00
Mathxxl <mathieu.baesen@free.fr>
2022-10-16 18:41:58 -0400

    Update ParticuleFriction.cpp

--- Projet_Maths_Physiques/src/Forces/ParticuleFriction.cpp
+++ Projet_Maths_Physiques/src/Forces/ParticuleFriction.cpp
@@ -7,5 +7,12 @@ ParticuleFriction::ParticuleFriction(const Vecteur3D& f = Vecteur3D(0,0,0)) {
 ParticuleFriction::~ParticuleFriction() {}
 
 void ParticuleFriction::updateForce(Particule* particule, float duration) {
-	particule->addForce();
+	double v = particule->getVelocity();
+	//WARNING : Intégration avec des forces nulles pour le moment
+	if (v == 0) { //Cas friction statique
+		particule->addForce(Vecteur3D(0,0,0));
+	}
+	else { //Cas friction cinétique
+		particule->addForce(Vecteur3D(0,0,0));
+	}
 }
