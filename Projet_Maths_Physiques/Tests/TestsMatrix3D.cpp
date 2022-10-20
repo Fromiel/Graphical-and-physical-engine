7c50571f5d6db170533a2cf32c364242a696c728
Fromiel <79905976+Fromiel@users.noreply.github.com>
2022-10-20 15:49:46 -0400

    Tests avec google test sur le cmakelist + deplacer le main dans dossier Game

---
+++ Projet_Maths_Physiques/Tests/TestsMatrix3D.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+#include "Matrix3D.h"
+
+TEST(addition, TEST1)
+{
+	Matrix3D result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }) + Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+	Matrix3D expected = Matrix3D({ 2, 4, 6, 8, 10, 12, 14, 16, 18 });
+	EXPECT_TRUE(result == expected);
+}
+
+
+TEST(substraction, TEST1)
+{
+	Matrix3D result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }) - Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+	Matrix3D expected = Matrix3D({ 0, 0, 0, 0, 0, 0, 0, 0,0 });
+	EXPECT_TRUE(result == expected);
+}
+
+TEST(multiplicationScalar, TEST1)
+{
+	Matrix3D result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }) * 2;
+	Matrix3D expected = Matrix3D({ 2, 4, 6, 8, 10, 12, 14, 16, 18 });
+	EXPECT_TRUE(result == expected);
+}
+
+TEST(multiplicationMatrix, TEST1)
+{
+	Matrix3D result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }) * Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+	Matrix3D expected = Matrix3D({ 30, 36, 42, 66, 81, 96, 102, 126, 150 });
+	EXPECT_TRUE(result == expected);
+}
+
+TEST(determinant, TEST1)
+{
+	double result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }).determinant();
+	double expected = 0;
+	EXPECT_TRUE(result == expected);
+}
+
+TEST(transpose, TEST1)
+{
+	Matrix3D result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }).transpose();
+	Matrix3D expected = Matrix3D({ 1, 4, 7, 2, 5, 8, 3, 6, 9 });
+	EXPECT_TRUE(result == expected);
+}
+
+TEST(adjugate, TEST1)
+{
+	Matrix3D result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }).adjugate();
+	Matrix3D expected = Matrix3D({ -3, 6, -3, 6, -12, 6, -3, 6, -3 });
+	EXPECT_TRUE(result == expected);
+}
+
+
+TEST(invert, TEST1)
+{
+	Matrix3D result = Matrix3D({ 1, 2, 3, 4, 5, 6, 7, 8, 8 }).invert();
+	Matrix3D expected = Matrix3D({ (double)-8 / 3, (double)8 / 3, -1, (double)10 / 3, (double)-13 / 3, 2, -1, 2, -1 });
+	EXPECT_TRUE(result == expected);
+}
+
+TEST(identity, TEST1)
+{
+	Matrix3D result = Matrix3D::identity();
+	Matrix3D expected = Matrix3D({ 1, 0, 0, 0, 1, 0, 0, 0, 1 });
+	EXPECT_TRUE(result == expected);
+}
+
