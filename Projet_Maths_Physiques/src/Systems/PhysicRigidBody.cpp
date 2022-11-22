7d3bd51ebf80ca0f5dfe8739f5cf2a27f6b52e7d
Mathxxl <mathieu.baesen@free.fr>
2022-11-22 16:56:20 -0500

    Debugging program

--- Projet_Maths_Physiques/src/Systems/PhysicRigidBody.cpp
+++ Projet_Maths_Physiques/src/Systems/PhysicRigidBody.cpp
@@ -7,7 +7,17 @@ void PhysicRigidBody::update(float dt)
 	Coordinator* coordinator = Coordinator::getInstance();
 
 	for (auto gameObject : entities_) {
-		coordinator->getComponent<Rigidbody>(gameObject).Integrate(dt);
-		coordinator->getComponent<Rigidbody>(gameObject).clearAccumulator();
+
+		std::cout << "GameObject : ";
+		std::cout << "Has component ? " << (coordinator->hasComponent<Rigidbody>(gameObject) ? "oui" : "non") << std::endl;
+
+
+		if (coordinator->hasComponent<Rigidbody>(gameObject)) {
+
+			std::cout << "In If condition" << std::endl;
+
+			coordinator->getComponent<Rigidbody>(gameObject).Integrate(dt);
+			coordinator->getComponent<Rigidbody>(gameObject).clearAccumulator();
+		}
 	}
 }
